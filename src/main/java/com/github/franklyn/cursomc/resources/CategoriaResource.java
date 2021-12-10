fa6fbd847269b89f9faafb724227e6e1a5f3f980
Franklyn <franklyn1998@live.com>
2021-12-09 22:29:04 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/github/franklyn/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/github/franklyn/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package com.github.franklyn.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.franklyn.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
-	
+
 }
