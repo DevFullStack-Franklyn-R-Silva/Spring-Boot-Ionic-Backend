f4d7cc3fa5efe1ba5846ce0cf1f0a97485bc71fa
Franklyn <franklyn1998@live.com>
2021-12-10 13:10:21 -0300

    Cliente, TipoCliente, telefones e endereços - Parte 2

--- src/main/java/com/github/franklyn/cursomc/CursomcApplication.java
+++ src/main/java/com/github/franklyn/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.github.franklyn.cursomc.domain.Categoria;
 import com.github.franklyn.cursomc.domain.Cidade;
+import com.github.franklyn.cursomc.domain.Cliente;
+import com.github.franklyn.cursomc.domain.Endereco;
 import com.github.franklyn.cursomc.domain.Estado;
 import com.github.franklyn.cursomc.domain.Produto;
+import com.github.franklyn.cursomc.domain.enums.TipoCliente;
 import com.github.franklyn.cursomc.repositories.CategoriaRepository;
 import com.github.franklyn.cursomc.repositories.CidadeRepository;
+import com.github.franklyn.cursomc.repositories.ClienteRepository;
+import com.github.franklyn.cursomc.repositories.EnderecoRepository;
 import com.github.franklyn.cursomc.repositories.EstadoRepository;
 import com.github.franklyn.cursomc.repositories.ProdutoRepository;
 
@@ -35,6 +40,13 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CidadeRepository cidadeRepository;
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
+	
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -71,6 +83,18 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2, est3));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "49247627095", TipoCliente.PESSOA_FISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("999983658", "912365248"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 
 }
---
+++ src/main/java/com/github/franklyn/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.github.franklyn.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.franklyn.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
