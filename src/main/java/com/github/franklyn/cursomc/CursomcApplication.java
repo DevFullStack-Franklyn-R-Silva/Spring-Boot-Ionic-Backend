164a5914369658df9444b706c3f547e2f52823ec
Franklyn <franklyn1998@live.com>
2021-12-10 08:41:20 -0300

    Criando operação de instanciação

--- src/main/java/com/github/franklyn/cursomc/CursomcApplication.java
+++ src/main/java/com/github/franklyn/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.github.franklyn.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.franklyn.cursomc.domain.Categoria;
+import com.github.franklyn.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
