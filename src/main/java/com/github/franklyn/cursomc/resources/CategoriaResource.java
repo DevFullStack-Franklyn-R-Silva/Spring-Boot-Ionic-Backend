ec52cf611be2bfcfcc3a00c0eaba3f4b1299e733
Franklyn <franklyn1998@live.com>
2021-12-14 16:35:23 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/github/franklyn/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/github/franklyn/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.github.franklyn.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.franklyn.cursomc.domain.Categoria;
 import com.github.franklyn.cursomc.services.CategoriaService;
@@ -23,4 +27,11 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
