3ba2a846a6eb25fd22b6ee31850d54af77de93aa
Franklyn <franklyn1998@live.com>
2021-12-14 17:48:03 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/github/franklyn/cursomc/services/CategoriaService.java
+++ src/main/java/com/github/franklyn/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.github.franklyn.cursomc.domain.Categoria;
@@ -42,8 +45,15 @@ public class CategoriaService {
 			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return categoriaRepository.findAll(pageRequest);
+	}
+
 }
