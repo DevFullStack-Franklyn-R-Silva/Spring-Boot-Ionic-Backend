814e6533c460b3ed500e05d204e47a9303fe3bf6
Franklyn <franklyn1998@live.com>
2021-12-27 11:22:09 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/github/franklyn/cursomc/services/CategoriaService.java
+++ src/main/java/com/github/franklyn/cursomc/services/CategoriaService.java
@@ -34,8 +34,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repository.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -60,4 +61,7 @@ public class CategoriaService {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
+	private void updateData(Categoria newObj , Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
 }
