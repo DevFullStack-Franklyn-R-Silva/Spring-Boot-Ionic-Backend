493b5296cf3d0ccf1a71751f3c254619aadab613
Franklyn <franklyn1998@live.com>
2021-12-28 12:15:32 -0300

    Inserindo Pedido

--- src/main/java/com/github/franklyn/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/github/franklyn/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.github.franklyn.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.github.franklyn.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/github/franklyn/cursomc/services/PedidoService.java
+++ src/main/java/com/github/franklyn/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.github.franklyn.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.github.franklyn.cursomc.domain.ItemPedido;
+import com.github.franklyn.cursomc.domain.PagamentoComBoleto;
 import com.github.franklyn.cursomc.domain.Pedido;
+import com.github.franklyn.cursomc.domain.enums.EstadoPagamento;
+import com.github.franklyn.cursomc.repositories.ItemPedidoRepository;
+import com.github.franklyn.cursomc.repositories.PagamentoRepository;
 import com.github.franklyn.cursomc.repositories.PedidoRepository;
 import com.github.franklyn.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +22,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = pedidoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + " Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = pedidoRepository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
