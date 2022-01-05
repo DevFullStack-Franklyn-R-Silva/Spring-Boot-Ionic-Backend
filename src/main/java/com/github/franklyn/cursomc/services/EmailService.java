e40eb59bbd12824ec58188be1e1ac1ffbe64fcdb
Franklyn <franklyn1998@live.com>
2022-01-05 19:25:14 -0300

    Email HTML

--- src/main/java/com/github/franklyn/cursomc/services/EmailService.java
+++ src/main/java/com/github/franklyn/cursomc/services/EmailService.java
@@ -1,13 +1,18 @@
 package com.github.franklyn.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.github.franklyn.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
-	
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
